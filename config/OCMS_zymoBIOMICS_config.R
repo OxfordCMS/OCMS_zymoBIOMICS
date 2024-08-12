e4b9db14333244c07f14160c8303e349f1179aa5
nickilott <nicholas.ilott@kennedy.ox.ac.uk>
2024-08-12 11:32:47 +0100

    updated for kraken

--- config/OCMS_zymoBIOMICS_config.R
+++ config/OCMS_zymoBIOMICS_config.R
@@ -6,17 +6,19 @@
 ###################################################################
 
 # file with counts
-counts_table=""
+counts_table="species_counts.tsv"
 
 # annotations - choose from "NCBI" or "GTDB"
-annotations = "GTDB"
+annotations = "NCBI"
 
 # type of sequencing - choose from "16S" or "shotgun"
+# for shotgun this currently only works with outputs
+# from the kraken2 pipeline
 sequence_type = "shotgun"
 
 # which columns in the data correspond to standards. This should
 # be a regular R vector of column names e.g. c("sample1", "sample2")
-standards_columns=""
+standards_columns=c("zymo1.abundance", "zymo2.abundance")
 
 # define any cutoffs for true positive and composition analysis
 species_cutoff=0.01
